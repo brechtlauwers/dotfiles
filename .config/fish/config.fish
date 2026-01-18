038efb8f9708346d60f6d1bd4533ba55cdb55441
brechtlauwers <brechtlauwers02@gmail.com>
2026-01-18 18:17:48 +0100

    Added initial dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
---
+++ .config/hypr/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+waybar &
+hyprpaper &
+/usr/lib/xdg-desktop-portal-hyprland &
+gammastep -l 51.02:4.47 -t 6500:2800 -b 1.0:0.8 &
