81686bcd583516bb8341dfc9e7aaa8bbe9f8a796
Brecht Lauwers <brechtlauwers02@gmail.com>
2026-01-19 12:38:44 +0100

    Changes on laptop

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
