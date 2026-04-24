0efb7d24b5d9de1955576869ec2093395041b6f1
brechtlauwers <brechtlauwers02@gmail.com>
2026-04-24 09:18:56 +0200

    pc changes

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -23,12 +23,18 @@
    '(all-the-icons-dired auctex biblio citar-org-roam company-box consult
 			 dashboard diminish ef-themes flycheck helpful
 			 jinx kotlin-mode lsp-pyright lsp-ui magit
-			 marginalia mood-line orderless org-fragtog
-			 org-noter pdf-tools python-mode vertico
-			 yaml-mode yasnippet-snippets)))
+			 marginalia mood-line multiple-cursors
+			 orderless org-fragtog org-noter pdf-tools
+			 python-mode vertico yaml-mode
+			 yasnippet-snippets))
+ '(safe-local-variable-values '((org-cite-global-bibliography))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.1)))))
